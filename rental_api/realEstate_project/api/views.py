9fb740b0849de4a08d9b382a7000302202c9116e
Voidblocker <akintundeakinsulure@gmail.com>
2023-05-29 21:23:21 +0100

    fix: edited functional based views to class based views

--- rental_api/realEstate_project/api/views.py
+++ rental_api/realEstate_project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
 from djoser.views import UserViewSet as DjoserUserViewSet
 from .serializers import UserCreateSerializer
@@ -12,40 +13,45 @@ class UserViewSet(DjoserUserViewSet):
     queryset = Agent.objects.all()
     serializer_class = UserCreateSerializer
 
-@api_view(['POST'])
-def create_property(request):
-    serializer = PropertySerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save(owner=request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CreatePropertyView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def post(self, request):
+        serializer = PropertySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT'])
-def update_property(request, pk):
-    try:
-        property = Property.objects.get(pk=pk)
-    except Property.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class UpdatePropertyView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    if property.owner != request.user:
-        return Response(status=status.HTTP_403_FORBIDDEN)
+    def put(self, request, pk):
+        try:
+            property = Property.objects.get(pk=pk)
+        except Property.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = PropertySerializer(property, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if property.owner != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
-@api_view(['DELETE'])
-def delete_property(request, pk):
-    try:
-        property = Property.objects.get(pk=pk)
-    except Property.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = PropertySerializer(property, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if property.owner != request.user:
-        return Response(status=status.HTTP_403_FORBIDDEN)
+class DeletePropertyView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    property.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, pk):
+        try:
+            property = Property.objects.get(pk=pk)
+        except Property.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if property.owner != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        property.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
